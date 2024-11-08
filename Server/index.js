6f606e4a4a11916ddd3573b67d8ed2baa8174eb6
Bhumi2004 <115335044+Bhumi2004@users.noreply.github.com>
2024-11-08 10:28:48 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
+
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const UserModel = require("./models/User");
